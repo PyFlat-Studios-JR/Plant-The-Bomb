277bff68e9112f4dbc8bdea57de9fd5da48e8c9f
Jonas Schlick <jonasfschlick@gmail.com>
2023-11-22 22:14:19 +0100

    added even more placeholder code

--- src/engine/bombManager.py
+++ src/engine/bombManager.py
@@ -1,6 +1,7 @@
 class bombManager():
-    def __init__(self):
+    def __init__(self, world):
         self.explosion_waitlist = []
+        self.world = world
     def tick(self):
         if len(self.explosion_waitlist) > 0:
             #acutally perform the "exploding task"
--- src/engine/world.py
+++ src/engine/world.py
@@ -1,11 +1,32 @@
 import src.engine.block as block
 import src.engine.player as player
 import src.engine.bombManager as bombManager
+from PySide6.QtGui import QPainter
 class world():
     def __init__(self):
-        self.blocks = [[block.air(self) for x in range (25)] for y in range (25)]
+        self.blocks = [[block.air(self) for x in range (25)] for y in range (25)] #very good world right now :)
         self.background = None #background drawing
-        self.overlay = None #overlay drawing
+        self.overlay = [[None]*25 for y in range (25)] #overlay drawing
         self.script_loader = None #scriptLoader
         self.player = None #player
-        self.bomb_manager = None #bomb Manager
+        self.bomb_manager = bombManager.bombManager(self) #bomb Manager
+    def tick(self):
+        #try to explode any unexploded explosives
+        self.bomb_manager.tick()
+        #actually force the player to confine to tickorder
+        for coloumn in self.blocks:
+            for cell in coloumn:
+                if cell.is_tickable:
+                    cell.tickEvent()
+        
+    def paintEvent(self, painter: QPainter): #do the initialization from elsewhere :)
+        self.background.paintEvent(painter) #draw background
+        for coloumn in self.blocks:
+            for cell in coloumn:
+                if cell.is_tickable:
+                    cell.drawEvent(painter)
+        #actually have to write this entire code ...
+        for coloumn in self.overlay:
+            for cell in coloumn:
+                if cell.is_occupied:
+                    cell.drawEvent(painter)
