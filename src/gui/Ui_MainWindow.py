4b7219e25c68b4b9926c2f1de5c707a0e831b48e
Anonym2546 <johannes.25406@gmail.com>
2023-11-24 08:04:10 +0100

    gameWindow

--- src/gui/Ui_MainWindow.py
+++ src/gui/Ui_MainWindow.py
@@ -373,7 +373,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.stackedWidget.setCurrentIndex(0)
+        self.stackedWidget.setCurrentIndex(4)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
