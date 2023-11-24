7b75329cf48ec17472329ca35459847bd25b169e
Jonas Schlick <jonasfschlick@gmail.com>
2023-11-24 09:08:10 +0100

    added player movement

--- src/engine/world.py
+++ src/engine/world.py
@@ -1,3 +1,4 @@
+import time
 import src.engine.block as block
 import src.engine.player as player
 import src.engine.bombManager as bombManager
@@ -6,8 +7,9 @@ import src.engine.background as background
 import src.engine.enemy as enemy
 from src.compressor import compressor
 from PySide6.QtGui import QPainter
+from PySide6.QtCore import QTimer
 class world():
-    def __init__(self, file):
+    def __init__(self, application, file):
         self.blocks = [[block.air(self) for x in range (25)] for y in range (25)] #very good world right now :)
         self.background = background.checkerboard #background drawing
         self.overlay = [[overlayTile.overlayTile(self,(x,y)) for y in range (25)] for x in range (25)] #overlay drawing
@@ -15,6 +17,10 @@ class world():
         self.player = None #player
         self.bomb_manager = bombManager.bombManager(self) #bomb Manager
         self.load_file(file)
+        self.ticker = QTimer()
+        self.ticker.timeout.connect(self.tick)
+        self.win = application
+        self.ticker.start(50)
     def load_file(self, file):
         c = compressor()
         c.load(file)
@@ -37,14 +43,16 @@ class world():
                     case 6:
                         self.blocks[x][y] = enemy.enemy(self, (x,y))
     def tick(self):
+        start = time.time()
         #try to explode any unexploded explosives
         self.bomb_manager.tick()
         #actually force the player to confine to tickorder
         for coloumn in self.blocks:
             for cell in coloumn:
                 if cell.is_tickable:
-                    cell.tickEvent()
-        
+                    cell.onTick()
+        self.win.update()
+        print(f"tick completed in {round((time.time()-start)*1000,2)}ms")
     def paintEvent(self, painter: QPainter): #do the initialization from elsewhere :)
         self.background.paintEvent(painter) #draw background
         for coloumn in self.blocks:
