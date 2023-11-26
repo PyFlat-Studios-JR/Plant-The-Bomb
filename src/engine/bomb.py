335c0364fb3c0689b6c245e484036b54abd6b149
PyFlat-Studios-JS <jonasfschlick@gmail.com>
2023-11-26 21:00:27 +0100

    not much progress

--- src/engine/bomb.py
+++ src/engine/bomb.py
@@ -1,11 +1,38 @@
 import src.engine.entity as entity
 
 class bomb(entity.entity):
-    def __init__(self, world, pos):
+    def __init__(self, world, pos, type, timer, player):
         super().__init__(world, pos)
-        self.explosion = 0 #Explosion enum: 0=basic 1=dynamite 2=nuke
-        self.timer = 0
+        self.explosion = type #Explosion enum: 0=basic 1=dynamite 2=nuke
+        self.timer = timer #explosion timer in ticks
+        self.damage = 0 #explosion damage in units
+        self.mode = 0 #0: line, 1: square
+        self.range = 0 #range (distance for lines, x*x for squares)
+        self.exploded = False
+        self.allow_explosions = False
+        self.is_destructible = True
+        self.initStats(player)
+    def initStats(self, player):
+        match(self.explosion):
+            case 0:
+                self.damage = player.damage
+                self.range = player.range
+                self.mode = 0
+            case 1:
+                self.range = 5
+                self.damage = player.damage * 2
+                self.mode = 1
+            case 2:
+                self.range = 20
+                self.damage = player.damage * 5
+                self.mode = 1
     def onTick(self):
-        pass
+        self.timer -= 1
+        if self.timer <= 0:
+            if not self.exploded:
+                self.exploded = True
+                self.world.bomb_manager.schedule(self)
     def onDestroy(self):
+        self.timer = 0
+    def explode(self):
         pass
--- src/engine/bombManager.py
+++ src/engine/bombManager.py
@@ -1,10 +1,11 @@
 class bombManager():
     def __init__(self, world):
         self.explosion_waitlist = []
+        self.explosions = []
         self.world = world
     def tick(self):
         if len(self.explosion_waitlist) > 0:
             #acutally perform the "exploding task"
-            self.explosion_waitlist.pop(0)
+            self.explosion_waitlist.pop(0).explode()
     def schedule(self, bomb):
         self.explosion_waitlist.append(bomb)
