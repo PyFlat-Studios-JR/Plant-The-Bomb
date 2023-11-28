21c9b67520bd680eb4ff8b9c1ebc4bcf2bb2786a
Johannes <johannes.25406@gmail.com>
2023-11-28 18:47:44 +0100

    fix?

--- src/gui/inventoryReloader.py
+++ src/gui/inventoryReloader.py
@@ -4,10 +4,10 @@ import src.engine.textureLib as textureLib
 
 class inventoryReloader():
     def reloadInventoryIcons(ui):
-        ui.bomb_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(9)))
+        ui.range_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(9)))
         ui.bomb_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(11)))
-        ui.bomb_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(13)))
-        ui.bomb_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(15)))
-        ui.bomb_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(17)))
-        ui.bomb_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(20)))
-        ui.bomb_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(23)))
+        ui.health_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(13)))
+        ui.timebomb_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(15)))
+        ui.dynamite_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(17)))
+        ui.damage_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(20)))
+        ui.nuke_icon_btn.setIcon(QPixmap.fromImage(textureLib.textureLib.getTexture(23)))
